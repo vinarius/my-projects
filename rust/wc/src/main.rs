5dd2442b15de6407f373801509629a46e785d8b2
Mark Kraus <vindevccm@gmail.com>
2024-06-29 16:22:26 -0400

    setup rust wc

--- rust/wc/src/main.rs
+++ rust/wc/src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+/// Print newline, word, and byte counts for the given file,
+/// or read from stdin if no file is specified.
+#[derive(Parser)]
+struct MyArgs {
+    /// Print the number of bytes
+    #[arg(short)]
+    c: (),
+}
+
 fn main() {
+    let _args = MyArgs::parse();
     println!("Hello, world!");
 }
