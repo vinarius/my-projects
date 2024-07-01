06df45165b8eaa42366bba7a03bb88eba67a83fa
Mark Kraus <vindevccm@gmail.com>
2024-07-01 15:31:43 -0400

    rust wc complete project

--- rust/wc/README.md
+++ rust/wc/README.md
@@ -1,11 +1,27 @@
 # wc - word, line, character, and byte count. 
 
+### Run with --help to print usage
+```bash
+> cargo run -- --help
+```
+
 Sample file found in data/test.txt
 
 Options:
 - -c: outputs the number of bytes in a file
+- -l: outputs the number of lines in a file
+- -w: outputs the number of words in a file
+- -m: outputs the number of characters in a file
 
 ```bash
-> wc -c test.txt
+> cargo run -- -c test.txt
 342190 test.txt
+```
+
+If no file is specified, will read from stdin.
+
+ie
+```bash
+> echo "foo" | cargo run
+1 1 3
 ```
--- rust/wc/src/logic.rs
+++ rust/wc/src/logic.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io;
 use std::path::PathBuf;
 
 use clap::Parser;
@@ -7,21 +8,44 @@ pub fn build_output() -> Result<String, std::io::Error> {
     let Args {
         bytes,
         lines,
+        words,
+        characters,
         file_path,
     } = get_args();
 
+    let default = !bytes && !lines && !words && !characters;
     let input = parse_input(&file_path)?;
     let mut output = String::new();
 
-    if bytes == true {
+    if lines == true || default {
+        let line_count = input.lines().count();
+
+        output.push_str(line_count.to_string().as_str());
+        output.push_str(" ");
+    }
+
+    if words == true || default {
+        let mut word_count = 0;
+
+        for line in input.lines() {
+            word_count += line.split_whitespace().count();
+        }
+
+        output.push_str(word_count.to_string().as_str());
+        output.push_str(" ");
+    }
+
+    if bytes == true || default {
         let byte_count = input.len();
+
         output.push_str(byte_count.to_string().as_str());
         output.push_str(" ");
     }
 
-    if lines == true {
-        let line_count = input.lines().count();
-        output.push_str(line_count.to_string().as_str());
+    if characters == true && !default {
+        let character_count = input.chars().count();
+
+        output.push_str(character_count.to_string().as_str());
         output.push_str(" ");
     }
 
@@ -37,11 +61,17 @@ pub fn build_output() -> Result<String, std::io::Error> {
 /// wc - word, line, character, and byte count
 #[derive(Debug, Parser)]
 pub struct Args {
-    #[clap(short = 'c', help = "Output the bytes in a file")]
+    #[arg(short = 'c', help = "Output the number of bytes in a file")]
     pub bytes: bool,
 
-    #[clap(short = 'l', help = "Output the lines in a file")]
+    #[arg(short = 'l', help = "Output the number of lines in a file")]
     pub lines: bool,
+
+    #[arg(short = 'w', help = "Output the number of words in a file")]
+    pub words: bool,
+
+    #[arg(short = 'm', help = "Output the number of characters in a file")]
+    pub characters: bool,
 
     pub file_path: Option<PathBuf>,
 }
@@ -58,5 +88,14 @@ pub fn parse_input(file_path: &Option<PathBuf>) -> Result<String, std::io::Error
         return Ok(file_contents);
     }
 
-    todo!("read from stdin");
+    let mut stdin_input_buffer = String::new();
+
+    for line_result in io::stdin().lines() {
+        let line = line_result?;
+
+        stdin_input_buffer.push_str(&line);
+        stdin_input_buffer.push_str("\n");
+    }
+
+    Ok(stdin_input_buffer)
 }
